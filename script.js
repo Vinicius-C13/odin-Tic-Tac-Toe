e202136dab910e76956ad00674464d82e7fe8892
vinicius <viniciuscosta.us@gmail.com>
2022-05-04 15:41:35 -0300

    I did this app based on another code. Now, I'll try to put together this and the past code

--- script.js
+++ script.js
@@ -1,125 +1,131 @@
-//Cache
-let xTurn = true;
-let currentClass = 'x-class';
+"use strict";
 
-//Factory Function
-const Player = (name, symbolClass)=>{
-    const play = (cell)=> {
-        cell.classList.add(symbolClass);
-    };
-    const celebration = ()=> {return `${name} wins!`}
-    
-    return{play, celebration}
+const Player = (sign) => {
+
+    const getSign = () => {
+        return sign;
+    }
+
+    return {getSign};
 };
-const xPlayer = Player('X', 'x-class');
-const oPlayer = Player('O', 'o-class');
 
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
 
+    const setField = (index, sign) => {
+        if(index>board.length) return;
+        board[index] = sign;
+    };
 
-//Module to control the board
-const board = (()=> { 
-    const boardCells = Array.from(document.querySelectorAll('.cell'));
-    
-    //All winning combinations
-    const winningCombinations = [
-        [0, 1, 2],
-        [0, 3, 6],
-        [0, 4, 8],
-        [1, 4, 7],
-        [2, 4, 6],
-        [2, 5, 8],
-        [3, 4, 5],
-        [6, 7, 8]
-    ];
+    const getField = (index, sign) => {
+        if(index>board.length) return;
+        return board[index];;
+    };
 
-    boardCells.forEach((cell)=>{
-    cell.addEventListener('click', ()=>handleClick.makeMove(cell), {once: true});
+    const reset = () => {
+        for(let i=0; i < board.length; i++) {
+            board[i] = "";
+        }
+    };
+
+    return { setField, getField, reset}
+})();
+
+const displayController = (() => {
+    const cellElements = document.querySelectorAll('.cell');
+    const resultMessage = document.querySelector('#result-message');
+    const resetButton = document.querySelector('#reset-button');
+
+    cellElements.forEach((cell) => 
+        cell.addEventListener('click', (e) => {
+            if(gameController.getIsOver() || e.target.textContent !== "") return;
+            gameController.playRound(parseInt(e.target.dataset.index));
+            updateGameBoard();
+            console.log('foi');
+        })
+    );
+
+    resetButton.addEventListener('click', e => {
+        gameBoard.reset();
+        gameController.reset();
+        updateGameBoard();
     });
 
-    return {boardCells, winningCombinations}
-})();
-
-
-//Module that store all main functions
-const handleClick = (()=>{
-    
-    //Execute the play
-    const makeMove = (cell)=>{
-        if(xTurn){xPlayer.play(cell)}
-        else{oPlayer.play(cell)}
-
-        if(checkWin(currentClass)){
-            endGame('win')
-        } else if (checkDraw()){
-            endGame('draw')
-        } else {
-            changeTurns()
-        };
-        console.log(`${xTurn? "Bola":"Xiss"}`)
-    };
-
-    //Check if someone win
-    const checkWin = (currentClass)=>{
-        return board.winningCombinations.some(combination => {
-            return combination.every(index => {
-                return board.boardCells[index].classList.contains(currentClass)
-            })
-        })
-    }
-
-    //Check if draw
-    const checkDraw = ()=>{
-        return [...board.boardCells].every(cell => {
-            return cell.classList.contains('x-class') || cell.classList.contains('o-class');
-        })
-    }
-
-    //Change the Turns
-    const changeTurns = () => { 
-        xTurn = !xTurn;
-        if(xTurn){
-            currentClass = 'x-class';
+    const updateGameBoard = () => {
+        for(let i = 0; i < cellElements.length; i++){
+            cellElements[i].textContent = gameBoard.getField(i);
         }
-        else {
-            currentClass = 'o-class';
-        }
-        controlUI.igniteClassChange();
-    };
+    } 
 
-    //Finish the game
-    const endGame = (result)=>{
-        if(result == 'win') {
-            controlUI.showEndScreen(`${xTurn ? xPlayer.celebration() : oPlayer.celebration()}`);
+    const setResultMessage = (result)=>{
+        if(result === "draw") {
+            setMessageElement("It's a draw!")
         } else {
-            controlUI.showEndScreen("It's Draw");
+            setMessageElement(`Player ${result} has won!`)
         }
     }
 
-    //Public function return
-    return {makeMove, currentClass}
+    const setMessageElement = (message) => {
+        resultMessage.textContent = message;
+    };
+
+    return { setResultMessage}
 })();
 
-const controlUI = (() =>{
-    const changeClass = ()=>{
-        const board = document.querySelector('#board');
+const gameController = (() => {
+    const PlayerX = Player("X");
+    const PlayerO = Player("O");
+    let round = 1;
+    let isOver = false;
 
-        if(xTurn){
-            board.classList.remove('o');
-            board.classList.add('x');
-        } else {
-            board.classList.remove('x');
-            board.classList.add('o');
+    const playRound = (fieldIndex) => {
+        gameBoard.setField(fieldIndex, getCurrentPlayerSign());
+        if (checkWinner(fieldIndex)) {
+            displayController.setResultMessage(getCurrentPlayerSign());
+            console.log(`Player ${getCurrentPlayerSign()} wins!`);
+            isOver = true;
+            return;
         }
+        if (round === 9) {
+            displayController.setResultMessage("draw");
+            isOver = true;
+            return;
+        }
+        round++;
     };
 
-    const showEndScreen = (message)=>{
-        document.querySelector('.end-screen').style.display = 'flex';
-        document.querySelector('#result-message').textContent = `${message}`;
+    const getCurrentPlayerSign = () => {
+        return round % 2 === 1 ? PlayerX.getSign() : PlayerO.getSign();
+    }
+
+    const checkWinner = (fieldIndex) => {
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        return winConditions.filter((combination) => combination.includes(fieldIndex))
+        .some((possibleCombination) =>
+          possibleCombination.every(
+            (index) => gameBoard.getField(index) === getCurrentPlayerSign()
+          )
+        );
+    }
+
+    const getIsOver = () => {
+        return isOver;
     };
 
-    const igniteClassChange = ()=> changeClass();
+    const reset = () => {
+        round = 1;
+        isOver = false;
+    };
 
-    return {igniteClassChange, showEndScreen};
+    return { playRound, getIsOver, reset }
 })();
-
-controlUI.igniteClassChange();
